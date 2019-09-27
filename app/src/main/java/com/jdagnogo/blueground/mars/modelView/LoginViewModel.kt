511906147811db6b9bc06c80c4f3aef72d4916d9
RHZZ4097 <jeanfrancois.dagnogo@orange.com>
2019-09-27 11:51:09 +0200

    create frist tests

--- app/src/main/java/com/jdagnogo/blueground/mars/modelView/LoginViewModel.kt
+++ app/src/main/java/com/jdagnogo/blueground/mars/modelView/LoginViewModel.kt
@@ -1,14 +1,17 @@
 package com.jdagnogo.blueground.mars.modelView
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.jdagnogo.blueground.mars.R
 import com.jdagnogo.blueground.mars.api.model.LoginParameters
 import com.jdagnogo.blueground.mars.api.model.LoginResponse
-import com.jdagnogo.blueground.mars.model.LoginToken
 import com.jdagnogo.blueground.mars.data.repository.LoginRepository
 import com.jdagnogo.blueground.mars.data.repository.TokenRepository
+import com.jdagnogo.blueground.mars.model.LoginToken
+import com.jdagnogo.blueground.mars.utils.Result.Status
 import com.jdagnogo.blueground.mars.utils.Result
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -34,12 +37,12 @@ class LoginViewModel @Inject constructor(
         viewModelScope.launch {
             val result = loginRepository.login(LoginParameters(email, password))
             when (result.status) {
-                Result.Status.SUCCESS -> {
-                    val tokenInformations : LoginToken =(result.data as LoginResponse).token
+                Status.SUCCESS -> {
+                    val tokenInformations: LoginToken = (result.data as LoginResponse).token
                     saveToken(tokenInformations)
-                    login.value = Result(Result.Status.SUCCESS, "", "")
+                    login.value = Result(Status.SUCCESS, "", "")
                 }
-                Result.Status.ERROR -> login.value = Result(Result.Status.ERROR, "", result.message)
+                Status.ERROR -> login.value = Result(Status.ERROR, "", result.message)
             }
         }
     }
@@ -59,67 +62,69 @@ class LoginViewModel @Inject constructor(
         if (!isEmailSizeCorrect(email, context)) return
         if (!isEmailContainsYearOfBirth(email, context)) return
         if (!isEmailContainsCountry(email, context)) return
-        currentName.value = Result(Result.Status.SUCCESS, "", "")
+        sendValue(Result.Status.SUCCESS, "")
     }
 
-    private fun isEmailContainsCountry(email: String, context: Context): Boolean {
+    @VisibleForTesting
+    fun isEmailContainsCountry(email: String, context: Context): Boolean {
         val emailSplitAroundDot = email.split(DOT)
         val country = emailSplitAroundDot[1]
         if (country.length != COUNTRY_LENGHT) {
-            currentName.value = Result(
-                Result.Status.ERROR, null, context.getString(R.string.error_country)
-            )
+            sendValue(Status.ERROR, context.getString(R.string.error_country))
             return false
         } else return true
     }
 
-    private fun isEmailContainsAnAt(email: String, context: Context): Boolean {
+    @VisibleForTesting
+    fun isEmailContainsAnAt(email: String, context: Context): Boolean {
         val emailSplitAroundDot = email.split(AT)
         if (emailSplitAroundDot.size != 2) {
-            currentName.value =
-                Result(Result.Status.ERROR, null, context.getString(R.string.error_at))
+            sendValue(Status.ERROR, context.getString(R.string.error_at))
             return false
         } else return true
     }
 
-    private fun isEmailSizeCorrect(email: String, context: Context): Boolean {
+    @VisibleForTesting
+    fun isEmailSizeCorrect(email: String, context: Context): Boolean {
         val emailSplitAroundAt = email.split(AT)
         val firstChar = emailSplitAroundAt[0]
         if (firstChar.length !in MIN_CHAR_LENGHT..MAX_CHAR_LENGHT) {
-            currentName.value = Result(
-                Result.Status.ERROR,
-                "",
-                context.getString(R.string.error_size)
-            )
+            sendValue(Status.ERROR, context.getString(R.string.error_size))
             return false
         } else return true
     }
 
-    private fun isEmailContainsYearOfBirth(email: String, context: Context): Boolean {
+    @VisibleForTesting
+    fun isEmailContainsYearOfBirth(email: String, context: Context): Boolean {
         val year = email.substringAfter(AT).substringBefore(DOT)
         if (year.toIntOrNull() == null || year.length != 4) {
-            currentName.value = Result(
-                Result.Status.ERROR, null, context.getString(R.string.error_year_of_birth)
-            )
+            sendValue(Status.ERROR, context.getString(R.string.error_year_of_birth))
             return false
         } else return true
     }
 
-    private fun isEmailContainsADot(email: String, context: Context): Boolean {
+    @VisibleForTesting
+    fun isEmailContainsADot(email: String, context: Context): Boolean {
         val emailSplitAroundDot = email.split(DOT)
         if (emailSplitAroundDot.size != 2) {
-            currentName.value =
-                Result(Result.Status.ERROR, null, context.getString(R.string.error_dot))
+            sendValue(Status.ERROR, context.getString(R.string.error_dot))
             return false
         } else return true
     }
 
-    private fun isEmailEmpty(email: String, context: Context): Boolean {
+    @VisibleForTesting
+    fun isEmailEmpty(email: String, context: Context): Boolean {
         if (email.isEmpty()) {
-            currentName.value = Result(
-                Result.Status.ERROR, null, context.getString(R.string.error_empty_email)
-            )
+            sendValue(Status.ERROR, context.getString(R.string.error_empty_email))
             return true
         } else return false
+    }
+
+    @VisibleForTesting
+    fun sendValue(status: Status?, message: String?) {
+        if (status == null) return
+        currentName.value = Result(
+            status, null, message
+        )
     }
 }
--- app/src/test/java/com/jdagnogo/blueground/mars/modelView/UnitDetailsViewModelTest.kt
+++ app/src/test/java/com/jdagnogo/blueground/mars/modelView/UnitDetailsViewModelTest.kt
@@ -1,2 +1,79 @@
 package com.jdagnogo.blueground.mars.modelView
 
+import android.content.Context
+import android.content.Intent
+import androidx.lifecycle.MutableLiveData
+import com.jdagnogo.blueground.mars.R
+import com.jdagnogo.blueground.mars.data.repository.LoginRepository
+import com.jdagnogo.blueground.mars.data.repository.TokenRepository
+import com.jdagnogo.blueground.mars.utils.Result
+import com.jdagnogo.blueground.mars.utils.Result.Status
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
+import org.mockito.BDDMockito.*
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.powermock.api.mockito.PowerMockito
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+
+@PrepareForTest(LoginRepository::class, TokenRepository::class, Result::class, Result.Status::class, LoginViewModel::class)
+@RunWith(PowerMockRunner::class)
+class UnitDetailsViewModelTest {
+    companion object {
+        const val FAKE_STRING = "FAKE_STRING"
+    }
+
+    protected inline fun <reified T> any(): T = Mockito.any(T::class.java)
+
+    protected fun <T> anyOrNull(): T = Mockito.any()
+
+    protected inline fun <reified T> eq(value: T): T = ArgumentMatchers.eq(value)
+
+    private lateinit var sut: LoginViewModel
+
+    private lateinit var loginRepository: LoginRepository
+
+    private lateinit var tokenRepository: TokenRepository
+
+    @Mock
+    private lateinit var context: Context
+
+    @Mock
+    private lateinit var currentName: MutableLiveData<Result<String>>
+
+    @Before
+    fun setUp() {
+        loginRepository = PowerMockito.mock((LoginRepository::class.java))
+        tokenRepository = PowerMockito.mock((TokenRepository::class.java))
+        sut = spy(LoginViewModel(loginRepository, tokenRepository))
+    }
+
+    //region isEmailEmpty
+    @Test
+    fun testIsEmailEmpty_emailEmpty_true() {
+        val email = ""
+        val status = Status.ERROR
+        given(context.getString(R.string.error_empty_email)).willReturn(FAKE_STRING)
+        willDoNothing().given(sut).sendValue(status, FAKE_STRING)
+        given(sut.currentName).willReturn(currentName)
+        given(currentName.value).willReturn(Result(status,"",""))
+        val result = sut.isEmailEmpty(email, context)
+
+        assertThat(result, equalTo(true))
+    }
+
+    @Test
+    fun testIsEmailEmpty_emailNotEmpty_false() {
+        given(context.getString(R.string.error_empty_email)).willReturn(FAKE_STRING)
+
+        val result = sut.isEmailEmpty(FAKE_STRING, context)
+
+        assertThat(result, equalTo(false))
+    }
+    //end region
+}
